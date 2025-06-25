163274341959d72e2e545df25020aa3c9cd6f03e
BashCooler <den.tryaskin@mail.ru>
2025-06-26 01:50:45 +0500

    Fixed telegram issues
    Pretty sure it works fine with telegram web now as I so wanted it to be

--- README.md
+++ README.md
@@ -1,6 +1,22 @@
 # What is it?
 
-**Replacer** allows you to replace
+**Replacer** allows you to replace specific keyboard input in real time.
+
+# Quick start
+
+Follow these few steps after building or downloading this application to set everything up.
+
+1. Launch replacer.exe
+2. Right click on icon in system tray, then press `Edit hotkeys`
+3. Specify replaces in opened YAML file like this
+```YAML
+trigger_word_1 : replace_word_1
+trigger_word_2 : replace_word_2
+```
+4. Close application by pressing `Exit` in system tray menu
+5. Start again to apply changes
+
+Now you can type (for this example) `trigger_word_1` and after pressing `Space` it will be automatically replaced to `replace_word_1` (as specified in YAML earlier).
 
 # How to build
 
--- replacer.py
+++ replacer.py
@@ -1,12 +1,21 @@
+import time
 from os import path, startfile
 import yaml
 import keyboard as kb
 import pystray as tray
 import threading
+import functools
 from PIL import Image
 
 BASE_DIR = path.dirname(path.abspath(__file__))
 RULE_LOC = path.join(BASE_DIR, "src", "rules.yaml")
+
+
+def replace(trigger, replacement):
+    for _ in trigger + ' ':
+        kb.send('backspace')
+    time.sleep(0.01)
+    kb.write(replacement + ' ')
 
 
 def set_keyboard_listener():
@@ -15,7 +24,8 @@ def set_keyboard_listener():
         rules = yaml.safe_load(file)
     # Add hotkeys
     for key, val in rules.items():
-        kb.add_abbreviation(key, val + ' ')
+        callback = functools.partial(replace, key, val)
+        kb.add_word_listener(key, callback)
     # Await hotkeys
     kb.wait()
 
